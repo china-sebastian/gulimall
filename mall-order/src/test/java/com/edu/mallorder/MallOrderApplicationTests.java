6e233872a1f83cf368049a5341d541df344cd637
china-sebastian <13450770408@163.com>
2023-06-11 17:00:11 +0800

    项目初始结构创建

---
+++ mall-order/src/test/java/com/edu/mallorder/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.mallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
